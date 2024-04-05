5d2f87d5b4134b8494ee9a6b95cc9f335b91a470
Rerechan Store <139312654+Rerechan-Store@users.noreply.github.com>
2024-04-05 22:05:28 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,7 @@ if [ "${EUID}" -ne 0 ]; then
 		echo "You need to run this script as root"
 		exit 1
 fi
-if [ "$(systemd-detect-virt)" == "openvz" ]; then
-		echo "OpenVZ is not supported"
-		exit 1
-fi
+
 if [ -f "/etc/xray/domain" ]; then
 echo "Script Already Installed"
 #exit 1
